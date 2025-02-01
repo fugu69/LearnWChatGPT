fb01472836de33c7b21a01f5c4fc07a6e25e5488
Maximus Brutalis <dmitroid1sk@gmail.com>
2025-01-31 21:15:16 -0700

    Quick Sort functions prototypes created

--- sortDynamicArray.c
+++ sortDynamicArray.c
@@ -6,11 +6,16 @@
 // Learning Quick Sort Algorith with ChatGPT has failed.
 // Good video to start with: https://www.youtube.com/watch?v=0jDiBM68NGU 
 
-/* void swap(int *a, int *b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-} */
+
+// Functions prototypes
+
+
+void swap(int *a, int *b);                  // swap array's elements in place
+void quicksort(int array[], int length);    // wrapper-function
+void quicksort_recursion(int arr[], int low, int high);    /*  algo itself. 
+                                                            low is 1st element, high is the last element. */
+int partition(int arr[], int low, int high);
+
 
 
 int main(){
@@ -18,6 +23,8 @@ int main(){
     srand(time(NULL));
 
     long long elemsNumber = 0;
+
+
     printf("Enter a number: ");
     scanf("%lld", &elemsNumber);
 
@@ -28,16 +35,49 @@ int main(){
         return 1;
     }
 
+    printf("Initial array created: \n");
     for(int i = 0; i < elemsNumber; i++){
         arr[i] = rand() % 100 + 1;
+        printf("%d\n", arr[i]);
     }
 
+    quicksort(arr, elemsNumber);
 
-    int x = 5;
-    int y = 10;
-
-    swap(&x, &y);
-    printf("x = %d, y = %d", x, y);
+    for(int i = 0; i < elemsNumber; i++){
+        printf("%d\n", arr[i]);
+    }
 
     return 0;
+}
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void quicksort(int array[], int length){
+    quicksort_recursion(array, 0, length - 1);
+}
+
+void quicksort_recursion(int array[], int low, int high){
+    if(low < high){
+        int pivot_index = partition(array, low, high);  // function returns index of pivot
+        quicksort_recursion(array, low, pivot_index - 1);  //left portion of array
+        quicksort_recursion(array, pivot_index + 1, high); //right portion of array
+    }
+
+}
+
+int partition(int array[], int low, int high){
+    int pivot_value = array[high];
+
+    int i = low;
+
+    for (int j = low; j < high; j++){
+        if(array[j] <= pivot_value){
+            swap(&array[i], &array[j]);
+            i++;
+        }
+    }
 }
